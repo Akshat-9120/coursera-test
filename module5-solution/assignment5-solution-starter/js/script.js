8111e82f80238cf39f91b7e273605b11db7abd36
Akshat-9120 <68431221+Akshat-9120@users.noreply.github.com>
2020-08-21 11:56:14 +0530

    Module5 - Solution

--- module5-solution/assignment5-solution-starter/js/script.js
+++ module5-solution/assignment5-solution-starter/js/script.js
@@ -101,8 +101,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory('categories');
-
+      // var chosenCategoryShortName = ....
+        var chosenCategoryShortName=chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -114,17 +114,17 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      
-      var homeHtmlToInsertIntoMainPage =  insertProperty(
-          $dc.loadMenuCategories('L')
-        );
-
-
+      //
+      // var homeHtmlToInsertIntoMainPage = ....
+      chosenCategoryShortName="'"+chosenCategoryShortName+"'";
+      var homeHtmlToInsertIntoMainPage=insertProperty(homeHtml,
+                   "randomCategoryShortName",
+                   chosenCategoryShortName);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml(selector, html);
-
+      // ....
+      insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
